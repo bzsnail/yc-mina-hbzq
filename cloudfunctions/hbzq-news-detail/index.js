195ae17df77d00fbd166135780115281af60e4b7
liyancai <liyancai1986@163.com>
2019-03-06 12:24:04 +0800

    @

--- cloudfunctions/hbzq-news-detail/index.js
+++ cloudfunctions/hbzq-news-detail/index.js
@@ -41,7 +41,7 @@ const getNewsDetail = (str) => {
   let news = {
     title: r[1],
     time: r[2],
-    content: getContentObj2(dealNewsContent2(r[3]))
+    content: getContentObj(dealNewsContent(r[3]))
   }
 
   return news
@@ -55,66 +55,14 @@ const dealNewsContent = function (str) {
   let cont = str
 
   cont = cont.replace(/\<\/p\>/igm, '</p>\n')
-    .replace(/\<img[\s\S]*?src="(.+?)"[\s\S]*?\/\>/igm, '<img src="$1" />')
-    .replace(/\<strong\>(.*?)\<\/strong\>/igm, '$1')
-    .replace(/\<span[\s\S]*?\>(.*?)\<\/span\>/igm, '$1')
-    .replace(/(\<p\>)([\s\S]*?)(\<img src=".*" \/\>)([\s\S]*?)(\<\/p\>)/igm, '$1$2$5$3$1$4$5')
-    .replace(/\<strong\>(.*?)\<\/strong\>/igm, '$1')
-    .replace(/\<span[\s\S]*?\>(.*?)\<\/span\>/igm, '$1')
-    .replace(/\<p\>&nbsp;\<\/p\>/igm, '')
-    .replace(/\<p\>\<\/p\>/igm, '')
-    .replace(/\<br \/\>/igm, '')
-    .replace(/\<br\/\>/igm, '')
-    .replace(/\<a[\s\S]*?\>(.*?)\<\/a\>/igm, '$1')
-    .replace(/\<p\>([\s\S]*?)\<\/p\>/igm, '<YC>text_$1</YC>')
-    .replace(/\<img src="(.*?)" \/\>/igm, '<YC>image_$1</YC>')
-    .replace(/\<video src="(.*?)" \/\>/igm, '<YC>video_$1</YC>')
-    .replace(/\<h2\>([\s\S]*?)\<\/h2\>/igm, '<YC>H2_$1</YC>')
-
-    return cont
-}
-
-/**
- * 从文本中过滤出图片
- */
-const getContentObj = function (str) {
-
-  let list = []
-
-  const reg = /\<YC\>([\s\S]*?)\<\/YC\>/igm;
-
-  var r = ''
-  while (r = reg.exec(str)) {
-
-    let s = r[1]
-    let i = s.indexOf('_')
-
-    list.push({
-      "type": s.substring(0, i),
-      "value": s.substr(i + 1).trim()
-    })
-
-  }
-  return list
-}
-
-/**
- * 处理文章内容，替换图片和文本标签
- */
-const dealNewsContent2 = function (str) {
-
-  let cont = str
-
-  cont = cont.replace(/\<\/p\>/igm, '</p>\n')
     .replace(/\<\/div\>/igm, '</div>\n')
     .replace(/\<p[\s\S]*?\>/igm, '<p>')
     .replace(/\<div[\s\S]*?\>/igm, '<div>')
     .replace(/\<\!--[\s\S]*?--\>/igm, '')
     .replace(/\<img[\s\S]*?src="(.+?)"[\s\S]*?\/\>/igm, '<img src="$1" />')
-    .replace(/\<strong\>(.*?)\<\/strong\>/igm, '$1')
+    .replace(/\<strong\>([\s\S]*?)\<\/strong\>/igm, '$1')
     .replace(/\<span[\s\S]*?\>(.*?)\<\/span\>/igm, '$1')
-    .replace(/\<p\>([\s\S]*?)(\<img src=".*" \/\>)([\s\S]*?)\<\/p\>/igm, '<p>$1</p>$2<p>$3</p>')
-    .replace(/\<strong\>(.*?)\<\/strong\>/igm, '$1')
+    .replace(/\<strong\>([\s\S]*?)\<\/strong\>/igm, '$1')
     .replace(/\<span[\s\S]*?\>(.*?)\<\/span\>/igm, '$1')
     .replace(/\<p\>&nbsp;\<\/p\>/igm, '')
     .replace(/\<p\>\<\/p\>/igm, '')
@@ -133,7 +81,7 @@ const dealNewsContent2 = function (str) {
 /**
  * 从文本中过滤出图片
  */
-const getContentObj2 = function (str) {
+const getContentObj = function (str) {
 
   let list = []
 
